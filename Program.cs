2f475d5f064aa7997b229dacb1fed3b81233b0dc
Carol <carolsato99@gmail.com>
2022-11-09 16:00:43 -0300

    primerio commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int qntdNotas;
+double notas = 0, media;
+
+Console.WriteLine("*-----------------------------------------------------------------*");
+Console.WriteLine("                         SISTEMA DE NOTAS");
+Console.WriteLine("Digite a quantidade de notas deste trimeste:");
+qntdNotas = int.Parse(Console.ReadLine());
+Console.WriteLine("                    ***** INSERINDO NOTAS *****");
+for(int x = 0; x<qntdNotas; x++){
+    Console.WriteLine("Digite a " + (x+1) + "ª nota do aluno:");
+    double notaInserida = int.Parse(Console.ReadLine());
+    
+    notas = notas + notaInserida;
+}
+
+media = notas/qntdNotas;
+
+Console.WriteLine("                    ***** MÉDIA DO ALUNO *****");
+Console.WriteLine("Média do alino: " + media);
+
+
+
+Console.WriteLine("*-----------------------------------------------------------------*");
